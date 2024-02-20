38200da29743eabf002296480eee51f685e21b93
rishabh2030 <rishabh.sharma1@ssipmt.com>
2024-02-21 02:45:27 +0530

    first commit

---
+++ src/main/java/com/rishabh/sirma_java_assignment/SirmaJavaAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.rishabh.sirma_java_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirmaJavaAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SirmaJavaAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rishabh/sirma_java_assignment/SirmaJavaAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishabh.sirma_java_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SirmaJavaAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
